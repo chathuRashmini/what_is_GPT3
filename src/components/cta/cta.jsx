9354caabcd797f0a900b7367f37a2b43033f05ad
chathu_08 <chathu.rash97@gmail.com>
2021-12-19 17:12:53 +0530

    Set up the file structure of the project

---
+++ src/components/cta/cta.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './cta.css'
+
+const cta = () => {
+    return (
+        <div>
+            Call To Action
+        </div>
+    )
+}
+
+export default cta
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './navbar.css'
+
+const navbar = () => {
+    return (
+        <div>
+            NAVBAR
+        </div>
+    )
+}
+
+export default navbar
